77efbbdda75fe22557aec4217a4a8361102d3320
MePilot <nebra2012@gmail.com>
2021-04-28 17:19:11 +0300

    first commit

--- server/index.js
+++ server/index.js
@@ -6,11 +6,15 @@ const path = require('path');
 const {sendRemindEmail} = require('./emails/account')
 
 app.use(express.json())
-app.use(express.static(path.join(__dirname, 'client/build')));
-if(process.env.NODE_ENV === 'production') {  app.use(express.static(path.join(__dirname, 'client/build')));    
+//app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', (req, res) => {    res.sendFile(path.join(__dirname = 'client/build/index.html'));  })}
+if(process.env.NODE_ENV === 'production') {  
+    app.use(express.static('client/build'));    
 
+app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));  
+    })
+    }
 
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
