63e76b450742a261fd884ce01fc014d63cfdab13
MePilot <nebra2012@gmail.com>
2021-04-28 16:37:16 +0300

    first commit

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -11,10 +11,11 @@ export default function NavBar(props) {
     <Navbar bg="dark" variant="dark" expand="lg">
       <Navbar.Brand onClick={()=>history.push('/')}>Task Manager</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse>
+      <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link onClick={()=>history.push('/')}>Home</Nav.Link>
           <Nav.Link  onClick={()=>history.push('/about')}>About</Nav.Link>
+          {props.token ? <Nav.Link onClick={()=>history.push('/mytasks')}>My Tasks</Nav.Link> : null}
         </Nav>
         {!props.token ? <LogRes></LogRes> : <LoggedIn user={props.user} logOut={props.logOut}></LoggedIn>}
       </Navbar.Collapse>
