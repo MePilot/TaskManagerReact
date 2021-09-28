4475e0bc8e4ea55a3fd02cdbeaa140d267943b84
MePilot <nebra2012@gmail.com>
2021-09-28 18:41:53 +0300

    first commit

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 //app.use(express.static(path.join(__dirname, 'client/build')));
 
 if(process.env.NODE_ENV === 'production') {  
-    app.use(path.resolve(process.cwd(),'client','build'));    
+    app.use(express.static(path.resolve(process.cwd(),'client','build')));    
 
 app.get('*', (req, res) => {
         res.sendFile(path.resolve(process.cwd(),'client','build','index.html'));  
