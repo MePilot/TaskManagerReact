7c301df497b24af1fd61341129c73be8629f094e
MePilot <nebra2012@gmail.com>
2021-09-28 19:52:41 +0300

    first commit

--- server/index.js
+++ server/index.js
@@ -1,20 +1,20 @@
-
+const cron = require('node-cron')
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 const path = require('path');
 const {sendRemindEmail} = require('./emails/account')
 
-console.log('AAAAAAAAAAAAAAAAAAAA : '+ path.resolve(__dirname,'client','build','index.html'))
+console.log('AAAAAAAAAAAAAAAAAAAA : '+ path.resolve(process.cwd(),'client','build','index.html'))
 
 app.use(express.json())
-//app.fdfuse(express.static(path.join(__dirname, 'client/build')));
+//app.use(express.static(path.join(__dirname, 'client/build')));
 
 if(process.env.NODE_ENV === 'production') {  
-    app.use(express.static(path.join(__dirname, 'client/build')));    
+    app.use(express.static(path.join(__dirname, '/client/build')));    
 
 app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname,'client','build','index.html'));  
+        res.sendFile(path.join(process.cwd(),'client','build','index.html'));  
     })
     }
 
